03faa66aa769d739d09f6e4ca3bc423808c0d118
Ashok Kumar Reddymasi <AReddymasi@Portal01.nextgen.com>
2021-03-19 15:08:16 +0530

    Committing AdminUsersTest.py changes.

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
+import time
 
 driver = webdriver.Chrome(executable_path="C:/Users/areddymasi/PycharmProjects/orange-hrm/Drivers/chromedriver.exe")
 url = "https://opensource-demo.orangehrmlive.com/"
 username = "Admin"
 password = "admin123"
-system_username = "Test.User"
 
 txt_username_name = "txtUsername"
 txt_password_name = "txtPassword"
@@ -13,10 +14,6 @@ btn_login_id = "btnLogin"
 tab_Admin_id = "menu_admin_viewAdminModule"
 submenu_UserManagement_id = "menu_admin_UserManagement"
 submenu_Users_id = "menu_admin_viewSystemUsers"
-
-txt_SystemUsername_id = "searchSystemUser_userName"
-btn_Search_id = "searchBtn"
-lbl_NoRecordFound_xpath = "//*[@id='resultTable']/tbody/tr/td"
 
 driver.maximize_window()
 driver.get(url)
@@ -30,7 +27,15 @@ admin = driver.find_element_by_id(tab_Admin_id)
 user_management = driver.find_element_by_id(submenu_UserManagement_id)
 users = driver.find_element_by_id(submenu_Users_id)
 action.move_to_element(admin).move_to_element(user_management).move_to_element(users).click().perform()
-driver.find_element_by_id(txt_SystemUsername_id).send_keys(system_username)
-driver.find_element_by_id(btn_Search_id).click()
-label = driver.find_element_by_xpath(lbl_NoRecordFound_xpath).text
-print(label)
+#Scroll down by pixel value.
+#driver.execute_script("window.scrollBy(0,1000)")
+#Scroll down to the end of the page
+#driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+#Scroll down the page till element is visible
+element = driver.find_element_by_xpath("//a[text()='TestAdmin']")
+driver.execute_script("arguments[0].scrollIntoView();", element)
+time.sleep(2)
+driver.save_screenshot("./Screenshots/New_User_Added.png")
+#driver.get_screenshot_as_file("./Screenshots/New_User_Added.png")
+
+
--- TestCases/AdminUsersTest.py
+++ TestCases/AdminUsersTest.py
@@ -7,6 +7,7 @@ sys.path.append("C:/Users/areddymasi/PycharmProjects/orange-hrm")
 from POM.LoginPage import LoginPage
 from POM.DashboardPage import DashboardPage
 from POM.UsersPage import UsersPage
+from POM.AddUserPage import AddUser
 
 
 class AdminUsersTest(unittest.TestCase):
@@ -15,13 +16,17 @@ class AdminUsersTest(unittest.TestCase):
     username = "Admin"
     password = "admin123"
     system_username = "Test.User"
+    user_role = "Admin"
+    enter_employee_name = "Pa"
+    add_username = "Tester"
+    add_password = "tester@123"
 
     @classmethod
     def setUpClass(cls):
         cls.driver.maximize_window()
         cls.driver.get(cls.url)
 
-    def test_Login(self):
+    def test_01_Login(self):
         lp = LoginPage(self.driver)
         lp.setUsername(self.username)
         lp.setPassword(self.password)
@@ -30,15 +35,31 @@ class AdminUsersTest(unittest.TestCase):
         self.assertEqual("OrangeHRM", self.driver.title, "Webpage title is not matching")
         print(self.driver.title)
 
-    def test_NavigatetoUsers(self):
+    def test_02_NavigatetoUsers(self):
         dp = DashboardPage(self.driver)
         dp.clickAdminUsers()
 
-    def test_SearchForInvalidUser(self):
+    def test_03_SearchForInvalidUser(self):
         up = UsersPage(self.driver)
         up.enter_Username(self.system_username)
         up.click_Search()
+        time.sleep(2)
         up.label_NoRecordsFound()
+        time.sleep(1)
+        up.click_Reset()
+        time.sleep(1)
+        up.click_Add()
+
+    def test_04_AddUser(self):
+        au = AddUser(self.driver)
+        au.get_label_AddUser()
+        au.select_UserRole()
+        au.enter_EmployeeName()
+        au.enter_Username()
+        au.enter_Password()
+        au.enter_ConfirmPassword()
+        au.click_Save()
+        au.verify_AddedUser()
 
     @classmethod
     def tearDownClass(cls):
